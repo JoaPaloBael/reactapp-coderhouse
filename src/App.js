f971812ac40837fe8e93712da58a4fab0becd891
Johanna Palo Bael <johannapalo.profecoder@gmail.com>
2022-06-05 20:13:42 -0300

    Agregue el componente Navbar, no le pude agregar estilos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
+import Navbar from "./components/Navbar/Navbar";
 function App() {
   return (
-    // className es lo mismo que class. Lo que pasa es que class es palabra reservada de JS
-    // Todas las etiquetas se tienen que cerrar. incluso img. Puedo escribir <img/>
-    <div className="App">
-      <h1>Hola mundo!!!</h1>
-    </div>
+    <Navbar />
   );
 }
 
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+function Navbar (){
+    return(
+        <nav>
+        <a href="#">Inicio</a>
+        <a href="#">Productos</a>
+      </nav>
+    )
+}
+
+export default Navbar;
