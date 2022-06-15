0d335d86162e8f2dc47f70c3d8bfb4c06d7c2aa5
Johanna Palo Bael <joa.palobael@gmail.com>
2022-06-15 11:25:13 -0300

    Correcciones desafio ItemCount

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import "./style.scss";
 import Navbar from "./components/Navbar/Navbar";
-import CarouselProductos from "./components/CarouselProductos/CarouselProductos";
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
+import ItemCount from "./components/ItemListContainer/ItemCount/ItemCount";
 
 function App() {
+  const onAdd = (quantity) => {
+    console.log(`compraste ${quantity} unidades`);
+  }
   return (
     <div className="body">
     <Navbar />
-    <CarouselProductos/>
+    <ItemListContainer>
+        <ItemCount initial={1} stock={9} onAdd={onAdd}/>
+        <ItemCount initial={1} stock={7} onAdd={onAdd}/>
+        <ItemCount initial={1} stock={3} onAdd={onAdd}/>
+      </ItemListContainer>
     </div>
   );
 }
---
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function ItemListContainer(props) {
+
+  return (
+    <div className='ItemListContainer'>
+      {props.children}
+    </div>
+  )
+}
