0fd9202d2915a835f3008babd8e822c781d2d075
Johanna Palo Bael <joa.palobael@gmail.com>
2022-06-15 07:24:39 -0300

    Agregue children

--- src/components/CarouselProductos/BoxProductos/BoxProductos.jsx
+++ src/components/CarouselProductos/BoxProductos/BoxProductos.jsx
@@ -1,15 +1,10 @@
 import React from 'react';
-import ItemCount from './ItemCount/ItemCount';
 
-export default function BoxProductos() {
-  const onAdd = (quantity) => {
-    console.log(`compraste ${quantity} unidades`);
-  }
+export default function BoxProductos(props) {
+
   return (
     <div className='BoxProductos'>
-      <ItemCount initial={1} stock={9} onAdd={onAdd}/>
-      <ItemCount initial={1} stock={7} onAdd={onAdd}/>
-      <ItemCount initial={1} stock={3} onAdd={onAdd}/>
+      {props.children}
     </div>
   )
 }
--- src/components/CarouselProductos/CarouselProductos.jsx
+++ src/components/CarouselProductos/CarouselProductos.jsx
@@ -1,9 +1,18 @@
 import BoxProductos from "./BoxProductos/BoxProductos";
+import ItemCount from "./BoxProductos/ItemCount/ItemCount";
 
 export default function CarouselProductos() {
+  const onAdd = (quantity) => {
+    console.log(`compraste ${quantity} unidades`);
+  }
   return (
     <section className="CarouselProductos">
-        <BoxProductos/>
+        <BoxProductos>
+          <ItemCount initial={1} stock={9} onAdd={onAdd}/>
+          <ItemCount initial={1} stock={7} onAdd={onAdd}/>
+          <ItemCount initial={1} stock={3} onAdd={onAdd}/>
+        </BoxProductos>
+
     </section>
   )
 }
