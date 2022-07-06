26488eb560a679868781293c02c18f428db25b11
Johanna Palo Bael <joa.palobael@gmail.com>
2022-07-06 10:35:27 -0300

    Mejore la navegabilidad del carrito al index

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 export default function Cart() {
   return (
-    <div>Carrito de compras</div>
+    <>
+    <h1>Carrito de compras</h1>
+    <button><Link to="/">Seguir comprando</Link></button>
+    </>
   )
 }
